4eb80dab69994ee0fe37f1dae7fd593640530a23
Dan Tran <nmdt2@cam.ac.uk>
2023-08-01 16:59:38 +0800

    Update marie.py

--- marie.py
+++ marie.py
@@ -1,24 +1,23 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+from nl2sparql import Nl2SparqlModel
+
 
 class Marie:
     def __init__(
-            self, 
-            kg_endpoint: str = "http://178.128.105.213:3838/blazegraph/namespace/ontospecies/sparql"
-        ):
-        self.nl2sparql_model = None
-        self.rel_linker = None
+        self,
+        model_path: str = "google/flan-t5-base",
+        kg_endpoint: str = "http://178.128.105.213:3838/blazegraph/namespace/ontospecies/sparql",
+    ):
+        self.nl2sparql_model = Nl2SparqlModel(model_path)
 
         sparql = SPARQLWrapper(kg_endpoint)
         sparql.setReturnFormat(JSON)
         self.sparql = sparql
 
-
     def execute_kg(self, query: str):
         self.sparql.setQuery(query)
         return self.sparql.queryAndConvert()["results"]["bindings"]
 
-
     def get_answer(self, question: str):
         query = self.nl2sparql_model(question)
         return self.execute_kg(query)
-    
