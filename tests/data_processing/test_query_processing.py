f67587a3174570cf5ead686b16542554dc251d08
Dan Tran <nmdt2@cam.ac.uk>
2023-08-03 14:40:05 +0800

    Update test_query_processing.py

--- tests/data_processing/test_query_processing.py
+++ tests/data_processing/test_query_processing.py
@@ -1,7 +1,6 @@
 import pytest
 
 from marie.data_processing.query_processing import (
-    add_prefixes,
     decode_query,
     encode_query,
     remove_prefixes,
@@ -48,16 +47,6 @@ class TestQueryUtils:
     )
     def test_removePrefixes(self, query, expected):
         assert remove_prefixes(query) == expected
-
-    def test_addPrefixes(self):
-        query = "SELECT *\nWHERE {?s ?p ?o}\n"
-        expected = (
-            "PREFIX os: <http://www.theworldavatar.com/ontology/ontospecies/OntoSpecies.owl#>\n"
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
-            "SELECT *\nWHERE {?s ?p ?o}\n"
-        )
-        assert add_prefixes(query) == expected
 
     def test_preprocessQuery(self):
         pass
