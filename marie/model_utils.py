61cfd655f6761ceafecc7021d99dc7a6e56c7e87
Dan Tran <nmdt2@cam.ac.uk>
2023-08-07 10:18:49 +0800

    Update lora target modules for t5

--- marie/model_utils.py
+++ marie/model_utils.py
@@ -34,7 +34,7 @@ def get_model(model_args: ModelArguments):
             lora_alpha=model_args.lora_alpha,
             lora_dropout=model_args.lora_dropout,
             bias="none",
-            target_modules=["q_proj", "v_proj"],
+            target_modules=["q", "v"],
             task_type=TaskType.SEQ_2_SEQ_LM
         )
 
